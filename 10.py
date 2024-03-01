40ea2d455db69ae3e27865d359fe00dc0d3b4db9
LyaminEgor <159790958+LyaminEgor@users.noreply.github.com>
2024-03-02 02:11:50 +0700

    Delete 10.py

--- 10.py
+++
@@ -1,9 +0,0 @@
-python
-def convert_meters_to_miles(meters):
-    miles = meters / 1609.34
-    return miles
-
-distance_in_meters = 762
-distance_in_miles = convert_meters_to_miles(distance_in_meters)
-
-print(f"Фейт Коннорс пробежала {distance_in_miles} миль.")
